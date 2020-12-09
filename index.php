3911133c1831ceb360d8c50bfc49f88e30c867ad
Louis Ronald <louisronsonronald@gmail.com>
2020-12-09 21:58:36 +1000

    Added index.css in styles directory

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
+    <link href="./styles/index.css" rel="stylesheet" crossorigin="anonymous">
+
+
     <title>Product Catalogue - Soul 2 UR Feet Ltd.</title>
   </head>
   <body>
